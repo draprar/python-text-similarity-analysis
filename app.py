ebffed13007a4ac53d9586744fc168f9c995c069
Walery <waleron@gmail.com>
2025-01-21 19:21:00 +0100

    Refactor - app decomposed

--- app.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-from gui.gui import AppGUI
-from helpers.processing import ProcessLogic
-
-def start_app():
-    root = tk.Tk()
-    process_logic = ProcessLogic()
-    app = AppGUI(root, process_logic)
-    root.mainloop()
--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from app import start_app
+import tkinter as tk
+from gui.gui import AppGUI
+from helpers.processing import ProcessLogic
 
 if __name__ == "__main__":
-    start_app()
+    root = tk.Tk()
+    process_logic = ProcessLogic()
+    app = AppGUI(root, process_logic)
+    root.mainloop()
